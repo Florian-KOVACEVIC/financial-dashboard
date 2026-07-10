5eb66a697a30f0ce08445c527a57b5d9d2255cdc
Florian KOVACEVIC <florian.kovacevic00@gmail.com>
2026-07-10 22:52:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ Financial Dashboard - Application Tableau de Bord Multi-Actifs
 Auteur  : FloKov
 Stack   : Streamlit · yfinance · Pandas · NumPy · Plotly
 Usage   : streamlit run app.py 
-""" 
+"""
  
 # Application tableau de bord financier
 import json
